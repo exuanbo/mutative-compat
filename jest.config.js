59bff835dd529b41bb13533b8120d91661f6d2cb
exuanbo <exuanbo@protonmail.com>
2023-12-26 18:18:07 +0000

    test: collect coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@
 
 /** @type {JestConfig} */
 const config = {
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ["unused"],
   preset: "ts-jest/presets/js-with-ts",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 }
